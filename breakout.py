45f84e0180f6a679e9cfbf9986b92d298eb4a986
ajoohuhbir <ribhuhooja@gmail.com>
2023-06-19 19:26:30 +0530

    Game over and game win screens added

--- breakout.py
+++ breakout.py
@@ -28,7 +28,7 @@ user_impulse_per_millisecond = 0.01
 paddle_max_speed_per_millisecond = 1
 air_resistance_coefficient = user_impulse_per_millisecond/paddle_max_speed_per_millisecond
 
-gravity = 0.00025
+gravity = 0.0002
 
 last_movement_key_pressed = None
 
@@ -44,18 +44,27 @@ class Ball:
         self.x = x
         self.y = y
         self.x_vel = random.uniform(-0.05, 0.05)
-        self.y_vel = -0.33
+        self.y_vel = -0.28
         self.radius = 5
         self.color = (255,255,255)
     
     def update(self, delta_t):
+        global game_state
         self.y_vel += gravity*delta_t
+        if self.x_vel > 0.1:
+            self.x_vel = 0.1
+        elif self.x_vel < -0.1:
+            self.x_vel = -0.1
+
         self.y += self.y_vel
         self.x += self.x_vel
         self.paddle_collision()
         self.wall_collision()
         for block in blocks:
             self.block_collision(block)
+        
+        if self.y > GAME_HEIGHT + 2*self.radius:
+            game_state = "game over"
     
     def render(self): # It is somewhat clunky to deal with scaling here, instead of in render, but I don't have a cleaner solution
         pygame.draw.circle(screen, self.color, (scale_x(self.x), scale_y(self.y)), scale_y(self.radius))   
@@ -63,14 +72,18 @@ class Ball:
 
     def paddle_collision(self):
         if self.y >= paddle_y and self.y <= paddle_y + PADDLE_HEIGHT:
-            if self.x > paddle_x + 3*self.radius and self.x < paddle_x + PADDLE_WIDTH - 3*self.radius:
+            if self.x > paddle_x and self.x < paddle_x + PADDLE_WIDTH:
                 self.y_vel *= -1
-            elif self.x < paddle_x + 3*self.radius and self.x > paddle_x - self.radius:
-                self.y_vel *= -1
-                self.x_vel += paddle_x_vel/4 if paddle_x_vel < 0 else 0
-            elif self.x > paddle_x + PADDLE_WIDTH - 3*self.radius and self.x < paddle_x + PADDLE_WIDTH + self.radius:
-                self.y_vel *= -1
-                self.x_vel += paddle_x_vel/4 if paddle_x_vel > 0 else 0
+                self.x_vel += paddle_x_vel/20
+            
+            # if self.x > paddle_x + 3*self.radius and self.x < paddle_x + PADDLE_WIDTH - 3*self.radius:
+            #     self.y_vel *= -1
+            # elif self.x < paddle_x + 3*self.radius and self.x > paddle_x - self.radius:
+            #     self.y_vel *= -1
+            #     self.x_vel += paddle_x_vel/4 if paddle_x_vel < 0 else 0
+            # elif self.x > paddle_x + PADDLE_WIDTH - 3*self.radius and self.x < paddle_x + PADDLE_WIDTH + self.radius:
+            #     self.y_vel *= -1
+            #     self.x_vel += paddle_x_vel/4 if paddle_x_vel > 0 else 0
 
     def wall_collision(self):
         if self.x < self.radius and self.x_vel < 0:
@@ -146,20 +159,47 @@ def update(delta_t):
         ball.update(delta_t)
 
 def handle_input():
-    global game_exit, last_movement_key_pressed
+    global game_exit, last_movement_key_pressed, game_state
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_exit = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                last_movement_key_pressed = 'A'
-            if event.key == pygame.K_d:
-                last_movement_key_pressed = 'D'
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_a and last_movement_key_pressed == 'A':
-                last_movement_key_pressed = None
-            if event.key == pygame.K_d and last_movement_key_pressed == 'D':
-                last_movement_key_pressed = None
+        if game_state == "play": 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    last_movement_key_pressed = 'A'
+                if event.key == pygame.K_d:
+                    last_movement_key_pressed = 'D'
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_a and last_movement_key_pressed == 'A':
+                    last_movement_key_pressed = None
+                if event.key == pygame.K_d and last_movement_key_pressed == 'D':
+                    last_movement_key_pressed = None
+        elif game_state == "game over":
+             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    restart()
+        elif game_state == "game win":
+             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    restart()                   
+
+def msg_screen(msg, x, y, color = (255,255,255), size = 25, font = "arial"):
+    surf = pygame.font.SysFont(font, size).render(msg, True, color)
+    trect = surf.get_rect()
+    trect.center = x,y
+    screen.blit(surf, trect)
+
+def restart():
+    global game_state, paddle_x, balls, paddle_x_vel, blocks, last_movement_key_pressed
+    balls = [Ball(GAME_WIDTH/2, paddle_y - 20)]
+    paddle_x = GAME_WIDTH/2 - PADDLE_WIDTH/2
+    paddle_x_vel = 0
+    blocks = []
+    for i in range(8):
+        for j in range(4):
+            blocks.append(Block(100*i + 2, 50*j +2, 95, 45, tuple((random.randint(0, 255) for i in range(3)))))
+    last_movement_key_pressed = None
+    game_state = "play"
 
 
 balls = [Ball(GAME_WIDTH/2, paddle_y - 20)]  # this should DEFINITELY not be global
@@ -168,13 +208,28 @@ for i in range(8):
     for j in range(4):
         blocks.append(Block(100*i + 2, 50*j +2, 95, 45, tuple((random.randint(0, 255) for i in range(3)))))
 
+game_state = "play"
+
 def GameLoop():
 
-    clock.tick()
+    global game_state
 
-    
     while not game_exit:
-
+        while game_state == "game over" and not game_exit:
+            screen.fill((0,0,0))
+            msg_screen("GAME OVER", RESOLUTION_WIDTH/2, RESOLUTION_HEIGHT/2)
+            msg_screen("Press R to restart", RESOLUTION_WIDTH/2, 0.6*RESOLUTION_HEIGHT)
+            handle_input()
+            pygame.display.update()
+            clock.tick(fps)
+        
+        while game_state == "game win" and not game_exit:
+            screen.fill((0,0,0))
+            msg_screen("YOU WIN!", RESOLUTION_WIDTH/2, RESOLUTION_HEIGHT/2)
+            msg_screen("Press R to restart", RESOLUTION_WIDTH/2, 0.6*RESOLUTION_HEIGHT)
+            handle_input()
+            pygame.display.update()
+            clock.tick(fps)        
 
         render()
         handle_input()
@@ -184,9 +239,9 @@ def GameLoop():
         for i in range(N):
             update(total_delta_t/N)
         
-        # print(total_delta_t)
-
-
+        if len(blocks) == 0:
+            game_state = "game win"
+        
         clock.tick(fps)
 
 
